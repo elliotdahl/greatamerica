393618fb5f49960c7e5a1c2c42306b1bf76be440
elliotdahl <edahl@pivotal.io>
2017-01-29 16:43:18 -0500

    basic layout

---
+++ source/javascripts/_vendor/basic.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+
+
+    console.log("american");
+    // var messages = ["Muslim", "Christian", "Jewish", "Atheist", "Buddhist"];
+    //
+    // function getMessage() {
+    //    return messages[Math.floor(Math.random() * messages.length)];
+    //    return messages[messages.length];
+    // }
+
+
+    function steps() {
+        console.log("step0");
+        $("#american").removeClass("red").text("Jewish");
+
+        setTimeout(
+            function() {
+                $("#american").text("Muslim");
+            }, 1000);
+        setTimeout(
+            function() {
+                $("#american").text("Buddhist");
+            }, 2000);
+        setTimeout(
+            function() {
+                $("#american").text("Christian");
+            }, 3000);
+        setTimeout(
+            function() {
+                $("#american").addClass("red").text("American");
+            }, 4000);
+        setTimeout(
+            function() {
+                $("#american").removeClass("red").text("Cisgender");
+            }, 5000);
+        setTimeout(
+            function() {
+                $("#american").text("Transgender");
+            }, 6000);
+        setTimeout(
+            function() {
+                $("#american").text("Homosexual");
+            }, 7000);
+        setTimeout(
+            function() {
+                $("#american").text("Undefined");
+            }, 8000);
+        setTimeout(
+            function() {
+                $("#american").addClass("red").text("American");
+            }, 9000);
+        setTimeout(
+            function() {
+                $("#american").removeClass("red").text("An Immigrant");
+            }, 10000);
+        setTimeout(
+            function() {
+                $("#american").text("A Refugee");
+            }, 11000);
+        setTimeout(
+            function() {
+                $("#american").text("A Minority");
+            }, 12000);
+        setTimeout(
+            function() {
+                $("#american").text("Homeless");
+            }, 13000);
+        setTimeout(
+            function() {
+                $("#american").addClass("red").text("American");
+            }, 14000);
+
+
+
+    }
+
+    steps();
+    var provisioning = setInterval(steps, 15000);
+
+
+
+
+
+    //$(document.body).append(message);
+
+
+
+});
